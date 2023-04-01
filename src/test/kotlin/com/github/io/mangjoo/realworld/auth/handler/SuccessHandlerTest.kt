44fbd621812099b4f950541cbd07d07682bb4a4b
Mang-Joo <wjwan0915@gmail.com>
2023-04-01 23:51:15 +0900

    :white_check_mark: test: token Fixture 및 SuccessHandler 테스트 작성

---
+++ src/test/kotlin/com/github/io/mangjoo/realworld/auth/handler/SuccessHandlerTest.kt
@@ -0,0 +1,43 @@
+package com.github.io.mangjoo.realworld.auth.handler
+
+import com.github.io.mangjoo.realworld.auth.domain.Role.*
+import com.github.io.mangjoo.realworld.auth.jwt.JwtCreate
+import com.github.io.mangjoo.realworld.fixture.Fixture
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.BDDMockito.given
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.springframework.mock.web.MockHttpServletResponse
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+
+@ExtendWith(MockitoExtension::class)
+class SuccessHandlerTest {
+
+    @InjectMocks
+    private lateinit var successHandler: SuccessHandler
+
+    @Mock
+    private lateinit var jwtCreate: JwtCreate
+
+    @Test
+    @DisplayName("성공 시 Jwt token을 response에 보내준다.")
+    fun `In case of success, Jwt token is sent in response`() {
+        //given
+        given(jwtCreate.createToken(1L, SimpleGrantedAuthority(ROLE_USER.name))).willReturn(Fixture().userToken)
+        val mockHttpServletResponse = MockHttpServletResponse()
+        val usernamePasswordAuthenticationToken = UsernamePasswordAuthenticationToken(1L, "password", listOf(SimpleGrantedAuthority(ROLE_USER.name)))
+
+        //when
+        successHandler.onAuthenticationSuccess(null, mockHttpServletResponse, usernamePasswordAuthenticationToken)
+
+        //then
+        assertThat(Fixture().userToken).isEqualTo(mockHttpServletResponse.contentAsString)
+    }
+
+}
--- src/test/kotlin/com/github/io/mangjoo/realworld/fixture/Fixture.kt
+++ src/test/kotlin/com/github/io/mangjoo/realworld/fixture/Fixture.kt
@@ -5,4 +5,5 @@ import com.github.io.mangjoo.realworld.auth.domain.UserEntity
 
 class Fixture {
     val userEntity = UserEntity(1L, "mangjoo@gmail.com", "password", "mangjoo", "bio", "image", true, ROLE_USER)
+    val userToken: String = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwicm9sZSI6IlJPTEVfVVNFUiJ9.n_Voazt4-FHnoVxrvnCSIDCepqK8cd-vEQYZLbsZ2Hc"
 }
