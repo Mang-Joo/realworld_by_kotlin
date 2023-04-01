8923016cf78e80bd7ed9aa0b94b8dafbd4cb0659
Mang-Joo <wjwan0915@gmail.com>
2023-04-01 23:50:51 +0900

    :sparkles: feat: authenticationManger Provider 추가 및 Bean 등록

--- src/main/kotlin/com/github/io/mangjoo/realworld/auth/config/SecurityConfig.kt
+++ src/main/kotlin/com/github/io/mangjoo/realworld/auth/config/SecurityConfig.kt
@@ -1,17 +1,24 @@
 package com.github.io.mangjoo.realworld.auth.config
 
+import com.github.io.mangjoo.realworld.auth.filter.JwtLoginFilter
+import com.github.io.mangjoo.realworld.auth.provider.JwtLoginProvider
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod.POST
 import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.ProviderManager
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy.STATELESS
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig {
+class SecurityConfig(
+    private val jwtLoginProvider: JwtLoginProvider
+) {
 
     @Bean
     fun configure(
@@ -27,7 +34,15 @@ class SecurityConfig {
         .anyRequest()
         .authenticated()
         .and()
+        .addJwtLoginFilter(authenticationManager)
         .build()
 
+    private fun HttpSecurity.addJwtLoginFilter(authenticationManager: AuthenticationManager): HttpSecurity = addFilterBefore(
+        JwtLoginFilter(AntPathRequestMatcher("/api/v1/login", POST.name()))
+            .apply { setAuthenticationManager(authenticationManager) },
+        UsernamePasswordAuthenticationFilter::class.java
+    )
 
+    @Bean
+    fun authenticationManager(): AuthenticationManager = ProviderManager(listOf(jwtLoginProvider))
 }
