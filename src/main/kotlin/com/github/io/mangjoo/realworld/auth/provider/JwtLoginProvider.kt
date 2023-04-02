9f6cc8e8344c5d7f47a9bd004a4f5daf842d8862
Mang-Joo <wjwan0915@gmail.com>
2023-04-02 23:21:41 +0900

    :sparkles: feat: 확장 함수 추가 및 클래스 비교 변경

--- src/main/kotlin/com/github/io/mangjoo/realworld/auth/provider/JwtLoginProvider.kt
+++ src/main/kotlin/com/github/io/mangjoo/realworld/auth/provider/JwtLoginProvider.kt
@@ -9,6 +9,9 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Component
 
+private val Authentication.credentialsByString: String
+    get() = this.credentials as String
+
 @Component
 class JwtLoginProvider(
     private val userDetailsService: JwtUserDetailsService,
@@ -18,13 +21,13 @@ class JwtLoginProvider(
         .let { it.principal as String }
         .let { userDetailsService.loadUserByUsername(it) }
         .apply {
-            passwordEncoder.matches(authentication.credentials as String, password)
+            passwordEncoder.matches(authentication.credentialsByString, password)
                 .takeIf { it }
                 ?: throw PasswordNotMatchException("Password not match")
         }
         .let { UsernamePasswordAuthenticationToken(it.id, null, listOf(SimpleGrantedAuthority(it.role.name))) }
 
     override fun supports(authentication: Class<*>): Boolean =
-        authentication.isAssignableFrom(JwtLoginProvider::class.java)
+        authentication == UsernamePasswordAuthenticationToken::class.java
 
 }
