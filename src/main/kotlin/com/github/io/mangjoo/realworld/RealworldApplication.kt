d7b78b4f19beaeeed2d1ac4dde12f600406a5432
Mang-Joo <wjwan0915@gmail.com>
2023-04-02 23:22:22 +0900

    :white_check_mark: test: CommandRunner 추가(로그인 위함)

--- src/main/kotlin/com/github/io/mangjoo/realworld/RealworldApplication.kt
+++ src/main/kotlin/com/github/io/mangjoo/realworld/RealworldApplication.kt
@@ -1,11 +1,37 @@
 package com.github.io.mangjoo.realworld
 
+import com.github.io.mangjoo.realworld.auth.domain.Role
+import com.github.io.mangjoo.realworld.auth.domain.UserEntity
+import com.github.io.mangjoo.realworld.auth.repository.UserRepository
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Component
 
 @SpringBootApplication
 class RealworldApplication
 
 fun main(args: Array<String>) {
     runApplication<RealworldApplication>(*args)
+}
+
+
+@Component
+class MyCommandLineRunner(
+    private val passwordEncoder: PasswordEncoder,
+    private val memberRepository: UserRepository
+) : CommandLineRunner {
+    override fun run(vararg args: String?) {
+        val memberEntity = UserEntity(
+            email = "mangjoo@gmail.com",
+            password = passwordEncoder.encode("1234"),
+            username = "mangjoo",
+            bio = "bio",
+            image = "image",
+            isEnabled = true,
+            role = Role.ROLE_USER
+        )
+        memberRepository.save(memberEntity)
+    }
 }
