6f163c3778e0e7be517c382352a951dd060aaf60
Mang-Joo <wjwan0915@gmail.com>
2023-04-02 23:20:33 +0900

    :sparkles: feat: UserRepository Save 추가

--- src/main/kotlin/com/github/io/mangjoo/realworld/auth/repository/UserRepository.kt
+++ src/main/kotlin/com/github/io/mangjoo/realworld/auth/repository/UserRepository.kt
@@ -3,16 +3,22 @@ package com.github.io.mangjoo.realworld.auth.repository
 import com.github.io.mangjoo.realworld.auth.domain.UserEntity
 import com.github.io.mangjoo.realworld.auth.exception.UserNotFoundException
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 
 interface UserRepository {
     fun findByEmail(email: String): UserEntity
 
+    fun save(userEntity: UserEntity): UserEntity
+
     @Component
+    @Transactional
     class UserRepositoryImpl(
         private val userJpaRepository: UserJpaRepository
     ) : UserRepository {
         override fun findByEmail(email: String): UserEntity =
             userJpaRepository.findByEmail(email)
                 ?: throw UserNotFoundException("$email not found")
+
+        override fun save(userEntity: UserEntity): UserEntity = userJpaRepository.save(userEntity)
     }
 }
