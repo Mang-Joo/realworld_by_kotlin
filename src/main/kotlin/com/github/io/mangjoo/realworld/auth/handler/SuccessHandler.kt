55ffb9b0bf357cafe9ab1b0f749a0c9a3b9dd4bf
kimwanjoo <wjwan0@hanpass.com>
2023-05-19 16:31:41 +0900

    :recycle: refactor: update variable name

--- src/main/kotlin/com/github/io/mangjoo/realworld/auth/handler/SuccessHandler.kt
+++ src/main/kotlin/com/github/io/mangjoo/realworld/auth/handler/SuccessHandler.kt
@@ -4,12 +4,12 @@ import com.github.io.mangjoo.realworld.auth.jwt.JwtCreate
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import jakarta.servlet.http.HttpServletResponse.SC_OK
-import java.nio.charset.StandardCharsets.UTF_8
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler
 import org.springframework.stereotype.Component
+import java.nio.charset.StandardCharsets.UTF_8
 
 private val HttpServletResponse?.checkResponse: HttpServletResponse
     get() = this ?: error("Response must not be null")
@@ -22,7 +22,7 @@ private val HttpServletResponse?.setSuccessResponseType: HttpServletResponse
             status = SC_OK
         }
 
-private val Authentication?.convertByPrcinpalLongId: Long
+private val Authentication?.convertByPrincipalLongId: Long
     get() = this?.let { it.principal as Long } ?: error("Authentication must not be null")
 
 private val Authentication?.convertByRole: GrantedAuthority
@@ -33,7 +33,7 @@ class SuccessHandler(
     private val jwtCreate: JwtCreate
 ) : AuthenticationSuccessHandler {
     override fun onAuthenticationSuccess(request: HttpServletRequest?, response: HttpServletResponse?, authentication: Authentication?) {
-        val jwt = jwtCreate.createToken(authentication.convertByPrcinpalLongId, authentication.convertByRole)
+        val jwt = jwtCreate.createToken(authentication.convertByPrincipalLongId, authentication.convertByRole)
         response.setSuccessResponseType
             .writer
             .write(jwt)
