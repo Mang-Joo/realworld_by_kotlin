75d74287eb343f08145d500fc200f69def6ec662
Mang-Joo <wjwan0915@gmail.com>
2023-05-21 23:53:37 +0900

    :sparkles: feat: add password encode and update response

--- src/main/kotlin/com/github/io/mangjoo/realworld/user/service/UserRegist.kt
+++ src/main/kotlin/com/github/io/mangjoo/realworld/user/service/UserRegist.kt
@@ -1,8 +1,10 @@
 package com.github.io.mangjoo.realworld.user.service
 
+import com.github.io.mangjoo.realworld.auth.config.JwtSupplier
 import com.github.io.mangjoo.realworld.user.domain.User
-import com.github.io.mangjoo.realworld.user.exception.UserDuplicationException
+import com.github.io.mangjoo.realworld.user.exception.UserException
 import com.github.io.mangjoo.realworld.user.repository.UserRepository
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -13,15 +15,18 @@ interface UserRegist {
     @Service
     @Transactional
     class UserRegistUsecase(
-        private val userRepository: UserRepository
+        private val userRepository: UserRepository,
+        private val passwordEncoder: PasswordEncoder,
+        private val jwtSupplier: JwtSupplier
     ) : UserRegist {
         override fun regist(registUseCaseRequest: RegistUseCaseRequest): UserResponse =
             if (userRepository.checkDuplication(registUseCaseRequest.email)) {
-                throw UserDuplicationException("${registUseCaseRequest.email} is already exists")
-            } else UserResponse.from(
-                userRepository.save(registUseCaseRequest.toEntity())
-            )
-
+                throw UserException("${registUseCaseRequest.email} is already exists")
+            } else {
+                val user =
+                    userRepository.save(registUseCaseRequest.toDomain(passwordEncoder.encode(registUseCaseRequest.password)))
+                UserResponse.from(user, jwtSupplier.supply(user))
+            }
     }
 
     data class RegistUseCaseRequest(
@@ -29,27 +34,29 @@ interface UserRegist {
         val password: String,
         val username: String
     ) {
-        fun toEntity(): User =
+        fun toDomain(encodePassword: String): User =
             User(
                 email = email,
-                password = password,
+                password = encodePassword,
                 username = username
             )
     }
 
     data class UserResponse(
-        val id: Long,
         val email: String,
-        val password: String,
-        val username: String
+        val token: String,
+        val username: String,
+        val bio: String,
+        val image: String,
     ) {
         companion object {
-            fun from(user: User): UserResponse =
+            fun from(user: User, token: String): UserResponse =
                 UserResponse(
-                    id = user.id,
                     email = user.email,
-                    password = user.password,
-                    username = user.username
+                    token = token,
+                    username = user.username,
+                    bio = user.bio,
+                    image = user.image,
                 )
         }
     }
