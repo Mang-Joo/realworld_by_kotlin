c9c32cd749f5f1caef3c0a47397f2e2f7fedd9b1
Mang-Joo <wjwan0915@gmail.com>
2023-05-22 17:06:48 +0900

    :sparkles: feat: find user api

--- src/main/kotlin/com/github/io/mangjoo/realworld/user/api/FindUserController.kt
+++ src/main/kotlin/com/github/io/mangjoo/realworld/user/api/FindUserController.kt
@@ -1,18 +1,39 @@
 package com.github.io.mangjoo.realworld.user.api
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal
+import com.github.io.mangjoo.realworld.user.domain.vo.UserInfo
+import com.github.io.mangjoo.realworld.user.service.FindUser
+import org.slf4j.LoggerFactory.*
+import org.springframework.http.ResponseEntity
+import org.springframework.security.oauth2.jwt.JwtDecoder
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
-import java.util.Objects
 
 @RestController
-class FindUserController {
-
-    private val logger = org.slf4j.LoggerFactory.getLogger(javaClass)
-
+class FindUserController(
+    private val findUser: FindUser,
+    private val jwtDecoder: JwtDecoder
+) {
     @GetMapping("/api/user")
-    fun findUser(@AuthenticationPrincipal principal: Objects) {
-        logger.info("principal: $principal")
-    }
+    fun findUser(@RequestHeader("Authorization") authorization: String) = jwtDecoder
+        .decode(authorization.removePrefix("Bearer ")).subject
+        .let { findUser.findUser(it.toLong()) }
+        .let { ResponseEntity.ok(FindUserResponse.from(it)) }
 
+    data class FindUserResponse(
+        val email: String,
+        val userName: String,
+        val bio: String,
+        val image: String,
+    ){
+        companion object{
+            fun from(userInfo: UserInfo) =
+                FindUserResponse(
+                    email = userInfo.email,
+                    userName = userInfo.username,
+                    bio = userInfo.bio,
+                    image = userInfo.image
+                )
+        }
+    }
 }
