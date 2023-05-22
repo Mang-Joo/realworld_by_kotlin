16db6067c0ec20faf680abc28332443809b8aba0
Mang-Joo <wjwan0915@gmail.com>
2023-05-22 17:07:34 +0900

    :white_check_mark: test: User domain update test

---
+++ src/test/kotlin/com/github/io/mangjoo/realworld/user/domain/UserTest.kt
@@ -0,0 +1,40 @@
+package com.github.io.mangjoo.realworld.user.domain
+
+import com.github.io.mangjoo.realworld.fixture.Fixture
+import com.github.io.mangjoo.realworld.user.domain.vo.UserInfo
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+
+class UserTest {
+
+    @Test
+    fun updateUserInfoTest() {
+        // Given
+        val user = Fixture().user
+        val update = UserInfo(
+            email = "update@update.com",
+            username = "update",
+            bio = "update",
+            image = "update",
+        )
+        // When
+        val updateUserInfo = user.updateUserInfo(update)
+
+        // Then
+        assertThat(update).isEqualTo(updateUserInfo.userInfo)
+    }
+
+    @Test
+    fun updatePasswordTest() {
+        // Given
+        val user = Fixture().user
+        val updatePassword = "updatePassword"
+
+        // When
+        val updateUserPassword = user.updatePassword(updatePassword)
+
+        // Then
+        assertThat(updateUserPassword.password).isEqualTo(updatePassword)
+    }
+}
