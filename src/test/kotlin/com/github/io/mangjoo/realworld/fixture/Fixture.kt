09fa7905b39e401cfe54c09024dede44222d33d6
Mang-Joo <wjwan0915@gmail.com>
2023-05-23 17:59:13 +0900

    :white_check_mark: test: add fixture data

--- src/test/kotlin/com/github/io/mangjoo/realworld/fixture/Fixture.kt
+++ src/test/kotlin/com/github/io/mangjoo/realworld/fixture/Fixture.kt
@@ -1,11 +1,21 @@
 package com.github.io.mangjoo.realworld.fixture
 
-import com.github.io.mangjoo.realworld.user.domain.Role.ROLE_USER
+import com.github.io.mangjoo.realworld.user.domain.Follow
 import com.github.io.mangjoo.realworld.user.domain.User
+import com.github.io.mangjoo.realworld.user.domain.vo.Role.ROLE_USER
 
 class Fixture {
+
+    val follower = User("follower@gmail.com", "password", "follower")
+        .apply { id = 2 }
+
     val user = User("mangjoo@gmail.com", "password", "mangjoo", "bio", "image", true, ROLE_USER)
-        .apply { id = 1 }
+        .apply {
+            id = 1
+            val follow = Follow(follower = follower, following = this, id = 1)
+            followers.add(follow)
+        }
+
     val userToken: String =
         "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwicm9sZSI6IlJPTEVfVVNFUiJ9.n_Voazt4-FHnoVxrvnCSIDCepqK8cd-vEQYZLbsZ2Hc"
 }
