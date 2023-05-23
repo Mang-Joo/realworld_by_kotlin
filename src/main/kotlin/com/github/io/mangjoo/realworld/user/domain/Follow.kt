0ca4d4f8f96d577884dd81b4226ad4a79bb9d89d
Mang-Joo <wjwan0915@gmail.com>
2023-05-23 18:01:07 +0900

    :sparkles: feat: add follow domain

---
+++ src/main/kotlin/com/github/io/mangjoo/realworld/user/domain/Follow.kt
@@ -0,0 +1,23 @@
+package com.github.io.mangjoo.realworld.user.domain
+
+import com.github.io.mangjoo.realworld.auth.common.BaseTimeEntity
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "follow")
+class Follow(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "follow_id")
+    var id: Long = 0,
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    @JoinColumn(name = "follower_id")
+    var follower: User,
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    @JoinColumn(name = "follow_id")
+    var following: User
+) : BaseTimeEntity() {
+
+}
