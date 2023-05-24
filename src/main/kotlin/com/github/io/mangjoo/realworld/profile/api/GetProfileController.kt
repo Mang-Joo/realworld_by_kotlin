69bb443cebd5ebd56525ab6f799e3486e1ea64e6
Mang-Joo <wjwan0915@gmail.com>
2023-05-24 13:41:09 +0900

    :recycle: refactor: add ResponseEntity

--- src/main/kotlin/com/github/io/mangjoo/realworld/profile/api/GetProfileController.kt
+++ src/main/kotlin/com/github/io/mangjoo/realworld/profile/api/GetProfileController.kt
@@ -1,6 +1,9 @@
 package com.github.io.mangjoo.realworld.profile.api
 
+import com.fasterxml.jackson.annotation.JsonRootName
 import com.github.io.mangjoo.realworld.profile.service.GetProfile
+import com.github.io.mangjoo.realworld.profile.vo.Profile
+import org.springframework.http.ResponseEntity
 import org.springframework.security.oauth2.jwt.JwtDecoder
 import org.springframework.web.bind.annotation.*
 
@@ -16,10 +19,22 @@ class GetProfileController(
         @PathVariable username: String
     ) =
         when (authorization?.removePrefix("Bearer ")) {
-            null -> getProfile.getProfile(username, null)
-            else -> getProfile.getProfile(
-                username,
-                jwtDecoder.decode(authorization.removePrefix("Bearer ")).subject.toLong()
-            )
+            null -> responseEntity(getProfile.getProfile(username, null))
+            else -> {
+                val id = jwtDecoder.decode(authorization.removePrefix("Bearer ")).subject
+                responseEntity(getProfile.getProfile(username, id.toLong()))
+            }
         }
+
+    private fun responseEntity(it: Profile) =
+        ResponseEntity.ok(GetProfileResponse(it.username, it.bio, it.image, it.following))
+
+
+    @JsonRootName("profile")
+    data class GetProfileResponse(
+        val username: String,
+        val bio: String?,
+        val image: String?,
+        val following: Boolean = false
+    )
 }
