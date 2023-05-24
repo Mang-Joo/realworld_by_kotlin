c0746b39b872751a0ea5f4f73253c16a475a24e2
Mang-Joo <wjwan0915@gmail.com>
2023-05-24 13:41:33 +0900

    :white_check_mark: test: add Fixture SignUp

--- src/test/kotlin/com/github/io/mangjoo/realworld/fixture/Fixture.kt
+++ src/test/kotlin/com/github/io/mangjoo/realworld/fixture/Fixture.kt
@@ -1,8 +1,14 @@
 package com.github.io.mangjoo.realworld.fixture
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.github.io.mangjoo.realworld.user.api.SignUpController.SignUpRequest
+import com.github.io.mangjoo.realworld.user.api.UserInfoResponse
 import com.github.io.mangjoo.realworld.user.domain.Follow
 import com.github.io.mangjoo.realworld.user.domain.User
 import com.github.io.mangjoo.realworld.user.domain.vo.Role.ROLE_USER
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.post
 
 class Fixture {
 
@@ -18,4 +24,17 @@ class Fixture {
 
     val userToken: String =
         "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwicm9sZSI6IlJPTEVfVVNFUiJ9.n_Voazt4-FHnoVxrvnCSIDCepqK8cd-vEQYZLbsZ2Hc"
+
+}
+
+fun MockMvc.singUpUser(
+    request: SignUpRequest = SignUpRequest("mangjoo@email.com", "password", "mangjoo"),
+): UserInfoResponse {
+    return post("/api/user") {
+        contentType = MediaType.APPLICATION_JSON
+        content = jacksonObjectMapper().writeValueAsString(request)
+    }.andReturn()
+        .response
+        .contentAsString
+        .let { jacksonObjectMapper().readValue(it, UserInfoResponse::class.java) }
 }
