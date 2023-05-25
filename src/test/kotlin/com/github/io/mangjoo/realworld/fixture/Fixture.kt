44c505b7ad0284dc9d3caf69ef250b5a48796ab6
Mang-Joo <wjwan0915@gmail.com>
2023-05-25 22:02:51 +0900

    :white_check_mark: test: update follow user

--- src/test/kotlin/com/github/io/mangjoo/realworld/fixture/Fixture.kt
+++ src/test/kotlin/com/github/io/mangjoo/realworld/fixture/Fixture.kt
@@ -3,7 +3,6 @@ package com.github.io.mangjoo.realworld.fixture
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.github.io.mangjoo.realworld.user.api.SignUpController.SignUpRequest
 import com.github.io.mangjoo.realworld.user.api.UserInfoResponse
-import com.github.io.mangjoo.realworld.user.domain.Follow
 import com.github.io.mangjoo.realworld.user.domain.User
 import com.github.io.mangjoo.realworld.user.domain.vo.Role.ROLE_USER
 import org.springframework.http.MediaType
@@ -12,17 +11,19 @@ import org.springframework.test.web.servlet.post
 
 class Fixture {
 
-    val follower = User("follower@gmail.com", "password", "follower")
-        .apply { id = 2 }
-
     val user = User("mangjoo@gmail.com", "password", "mangjoo", "bio", "image", true, ROLE_USER)
         .apply {
             id = 1
-            val follow = Follow(from = follower, to = this, id = 1)
-            followers.add(follow)
         }
+
+    val follower = User("follower@gmail.com", "password", "follower")
+        .apply {
+            id = 2
+            this.follow(user)
+        }
+
     val follower2 = User("followe22r@gmail.com", "password22", "follower22")
-    .apply { id = 3 }
+        .apply { id = 3 }
 
     val userToken: String =
         "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwicm9sZSI6IlJPTEVfVVNFUiJ9.n_Voazt4-FHnoVxrvnCSIDCepqK8cd-vEQYZLbsZ2Hc"
