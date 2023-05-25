cd7006f69b419a93c5cc5b4afc05d9b72ddcd465
Mang-Joo <wjwan0915@gmail.com>
2023-05-25 17:43:01 +0900

    :sparkles: test: rename typo and add Fixture user

--- src/test/kotlin/com/github/io/mangjoo/realworld/fixture/Fixture.kt
+++ src/test/kotlin/com/github/io/mangjoo/realworld/fixture/Fixture.kt
@@ -18,16 +18,18 @@ class Fixture {
     val user = User("mangjoo@gmail.com", "password", "mangjoo", "bio", "image", true, ROLE_USER)
         .apply {
             id = 1
-            val follow = Follow(follower = follower, following = this, id = 1)
+            val follow = Follow(from = follower, to = this, id = 1)
             followers.add(follow)
         }
+    val follower2 = User("followe22r@gmail.com", "password22", "follower22")
+    .apply { id = 3 }
 
     val userToken: String =
         "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwicm9sZSI6IlJPTEVfVVNFUiJ9.n_Voazt4-FHnoVxrvnCSIDCepqK8cd-vEQYZLbsZ2Hc"
 
 }
 
-fun MockMvc.singUpUser(
+fun MockMvc.signUpUser(
     request: SignUpRequest = SignUpRequest("mangjoo@email.com", "password", "mangjoo"),
 ): UserInfoResponse {
     return post("/api/user") {
