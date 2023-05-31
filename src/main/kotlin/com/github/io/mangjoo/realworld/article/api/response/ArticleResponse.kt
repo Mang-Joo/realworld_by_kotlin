10774eaf0ed474647aeea77848e4088c8452a99e
Mang-Joo <wjwan0915@gmail.com>
2023-06-01 00:17:01 +0900

    :sparkles: fix(article): articles response transform

--- src/main/kotlin/com/github/io/mangjoo/realworld/article/api/response/ArticleResponse.kt
+++ src/main/kotlin/com/github/io/mangjoo/realworld/article/api/response/ArticleResponse.kt
@@ -5,30 +5,30 @@ import com.github.io.mangjoo.realworld.user.domain.User
 import java.time.LocalDateTime
 
 data class ArticleResponse(
-    val slug: String,
-    val title: String,
-    val description: String,
-    val body: String,
-    val tagList: Set<String>,
-    val createdAt: LocalDateTime,
-    val updatedAt: LocalDateTime,
-    val favorited: Boolean,
-    val favoritesCount: Int,
-    val author: AuthorResponse
+        val slug: String,
+        val title: String,
+        val description: String,
+        val body: String,
+        val tagList: Set<String>,
+        val createdAt: LocalDateTime,
+        val updatedAt: LocalDateTime,
+        val favorited: Boolean,
+        val favoritesCount: Int,
+        val author: AuthorResponse
 ) {
     companion object {
         fun of(article: Article, user: User?): ArticleResponse {
             return ArticleResponse(
-                slug = article.id.toString(),
-                title = article.title,
-                description = article.description,
-                body = article.body,
-                tagList = article.tags(),
-                createdAt = article.createdDate,
-                updatedAt = article.modifiedDate,
-                favorited = user?.let { article.isFavorite(it.username) } ?: false,
-                favoritesCount = article.favoriteCount(),
-                author = AuthorResponse.of(article.author, user?.let { article.author.isFollowing(it) } ?: false)
+                    slug = article.id.toString(),
+                    title = article.title,
+                    description = article.description,
+                    body = article.body,
+                    tagList = article.tags(),
+                    createdAt = article.createdDate,
+                    updatedAt = article.modifiedDate,
+                    favorited = user?.let { article.isFavorite(it.username) } ?: false,
+                    favoritesCount = article.favoriteCount(),
+                    author = AuthorResponse.of(article.author, user?.isFollowing(article.author) ?: false)
             )
         }
     }
