2161d5a67399e2f79bf2e01a6eeec13867b87f6c
Mang-Joo <wjwan0915@gmail.com>
2023-06-01 00:14:20 +0900

    :sparkles: feat(auth): write jwt decoder

---
+++ src/main/kotlin/com/github/io/mangjoo/realworld/auth/jwt/JwtDecode.kt
@@ -0,0 +1,14 @@
+package com.github.io.mangjoo.realworld.auth.jwt
+
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.stereotype.Component
+
+@Component
+class JwtDecode(
+    private val jwtDecoder: JwtDecoder
+) {
+    fun tokenToUserId(token: String?): Long? = when(token) {
+        null -> null
+        else -> jwtDecoder.decode(token).subject.toLong()
+    }
+}
