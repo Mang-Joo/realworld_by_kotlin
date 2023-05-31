d11a3f202fd3a254c208023ce109cfdd63b02a77
Mang-Joo <wjwan0915@gmail.com>
2023-06-01 00:16:18 +0900

    :sparkles: feat(article): get articles api

--- src/main/kotlin/com/github/io/mangjoo/realworld/article/api/ArticleController.kt
+++ src/main/kotlin/com/github/io/mangjoo/realworld/article/api/ArticleController.kt
@@ -1,24 +1,36 @@
 package com.github.io.mangjoo.realworld.article.api
 
 import com.github.io.mangjoo.realworld.article.api.request.GetArticleRequest
-import org.springframework.data.domain.PageRequest
+import com.github.io.mangjoo.realworld.article.api.request.PageRequest
+import com.github.io.mangjoo.realworld.article.api.response.GetArticlesResponse
+import com.github.io.mangjoo.realworld.article.service.GetArticlesUseCase
+import com.github.io.mangjoo.realworld.auth.jwt.JwtDecode
 import org.springframework.data.web.PageableDefault
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ArticleController {
+class ArticleController(
+    private val getArticlesUseCase: GetArticlesUseCase,
+    private val jwtDecode: JwtDecode
+) {
 
     @GetMapping("/api/articles")
     fun getArticles(
         @ModelAttribute getArticleRequest: GetArticleRequest,
-        @PageableDefault pageRequest: PageRequest,
-        @RequestHeader("Authorization") token: String
-    ) {
-
-    }
+        @RequestHeader("Authorization") token: String?,
+        @PageableDefault pageRequest: PageRequest
+    ): ResponseEntity<GetArticlesResponse> = ResponseEntity.ok()
+        .body(
+            getArticlesUseCase.getArticles(
+                getArticleRequest.toFilterByArticles(),
+                pageRequest,
+                jwtDecode.tokenToUserId(token)
+            )
+        )
 
 
 }
