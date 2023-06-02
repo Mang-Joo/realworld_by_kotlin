6c365f62e60d6e5cedbb2731c441ab31313c6f12
Mang-Joo <wjwan0915@gmail.com>
2023-06-02 10:39:05 +0900

    :white_check_mark: test: article query test

--- src/test/kotlin/com/github/io/mangjoo/realworld/article/repository/ArticleJpaRepositoryTest.kt
+++ src/test/kotlin/com/github/io/mangjoo/realworld/article/repository/ArticleJpaRepositoryTest.kt
@@ -1,35 +1,46 @@
 package com.github.io.mangjoo.realworld.article.repository
 
 import com.github.io.mangjoo.realworld.article.domain.Article
-import com.github.io.mangjoo.realworld.fixture.Fixture
+import com.github.io.mangjoo.realworld.user.domain.User
+import com.github.io.mangjoo.realworld.user.domain.vo.UserInfo
 import com.github.io.mangjoo.realworld.user.repository.UserJpaRepository
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.data.domain.PageRequest
 
 @DataJpaTest
-//@Transactional
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ArticleJpaRepositoryTest(
     @Autowired private val articleJpaRepository: ArticleJpaRepository,
     @Autowired private val userRepository: UserJpaRepository
 ) {
     @Test
-    fun `findAllTest`() {
-        userRepository.save(Fixture().user)
+    fun findAllTest() {
+        val user = User(
+            password = "password",
+            userInfo = UserInfo(
+                username = "username",
+                email = "email",
+                bio = "bio",
+                image = "image"
+            )
+        )
+        val save = userRepository.save(user)
 
         val article = Article(
             title = "title",
             description = "description",
             body = "body",
-            author = Fixture().user,
+            author = save,
             tags = mutableSetOf("tag1", "tag2")
         )
         articleJpaRepository.save(article)
         val articles: Collection<Article> =
             articleJpaRepository.findAll(
                 tag = "tag1",
-                author = Fixture().user.username,
+                author = user.username,
                 favorited = null,
                 pageable = PageRequest.of(0, 10)
             )
